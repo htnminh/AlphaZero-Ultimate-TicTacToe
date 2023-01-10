c4db1211ff3d8c83254bce278c8364933169b25f
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2023-01-10 22:58:05 +0700

    done basic GUI game

--- ultimatettt/exceptions.py
+++ ultimatettt/exceptions.py
@@ -1,22 +1,26 @@
-class BoardWonException(Exception):
+class GameException(Exception):
+    pass
+
+
+class BoardWonException(GameException):
     def __init__(self, board):
         message = f'Player {board} already won, please restart the game'    
         super().__init__(message)
 
 
-class AreaWonException(Exception):
+class AreaWonException(GameException):
     def __init__(self, played_area):
         message = f'Area {played_area} already won'
         super().__init__(message)
 
 
-class AreaWrongException(Exception):
+class AreaWrongException(GameException):
     def __init__(self, next_area, played_area):
         message = f'The next area is {next_area}, player is not allowed to play on {played_area}'
         super().__init__(message)
         
 
-class CellPlayedException(Exception):
+class CellPlayedException(GameException):
     def __init__(self, xyij):      
         message = f'Cell {xyij} is already played'      
         super().__init__(message)
--- ultimatettt/gui.py
+++ ultimatettt/gui.py
@@ -2,8 +2,7 @@ import PySimpleGUI as sg
 import numpy as np
 
 from game import Utils, State
-from exceptions import \
-    BoardWonException, AreaWonException, AreaWrongException, CellPlayedException
+from exceptions import GameException
 
 
 X_CHAR = '\u274C'
@@ -11,13 +10,18 @@ O_CHAR = '\u26AB'
 X_COLOR = 'red'
 O_COLOR = 'blue'
 BACKGROUND_COLORS = ('darkgrey', 'white')
+X_WON_BACKGROUND_COLOR = 'lightcoral'
+O_WON_BACKGROUND_COLOR = 'cornflowerblue'
 WIDTH = 3
 FONT_SIZE = 25
 NEXTG_COLOR = 'lightgreen'
+X_TURN_STR = 'Player 1 turn as X'
+O_TURN_STR = 'Player 2 turn as O'
 
 
 class GraphicInterface():
     def __init__(self):
+        self.state = State()
         self.layout = self.create_layout()
         self.window = self.create_window('Ultimate Tic-Tac-Toe')
         self.event_loop()
@@ -39,7 +43,8 @@ class GraphicInterface():
                     )
                 )
         
-        layout.append([sg.Text('',key='text')])
+        layout.append([sg.Text(X_TURN_STR, key='textPlayerTurn')])
+        layout.append([sg.Text('', key='textException')])
         
         return layout
 
@@ -65,31 +70,43 @@ class GraphicInterface():
                     color_if_odd if (x+y)%2 else color_if_even
                 )
 
-    def event_loop(self):
-        state = State()
+    def play(self, xyij):
+        x, y, i, j = xyij 
 
+        try:
+            self.state.play((x, y, i, j))
+
+            # update played cell
+            self.update_button_text(
+                xyij, X_CHAR if self.state.next_player==2 else O_CHAR)
+            self.update_button_color(
+                xyij, X_COLOR if self.state.next_player==2 else O_COLOR, None)
+            # change color of played area to default or win color
+            state_of_area = self.state.area[x, y]
+            if state_of_area == 0:
+                self.update_area_color((x, y), None, *BACKGROUND_COLORS)
+            else:
+                bkg_color_won = [X_WON_BACKGROUND_COLOR] * 2 if state_of_area == 1 else [O_WON_BACKGROUND_COLOR] * 2
+                self.update_area_color((x, y), None, *bkg_color_won)
+            # change next area color
+            if self.state.next_area is not None:
+                self.update_area_color(self.state.next_area, None, NEXTG_COLOR, NEXTG_COLOR)
+            # update texts
+            self.window['textPlayerTurn'].update(X_TURN_STR if self.state.next_player==1 else O_TURN_STR)
+            self.window['textException'].update('')
+            
+        except GameException as e:
+            self.window['textException'].update(e)
+
+    def event_loop(self):
         while True:
             event, values = self.window.read()
 
             if event == sg.WIN_CLOSED:
                 break
             elif event.startswith('cell'):
-                x, y, i, j = xyij = tuple(map(int, event[-4:]))
-
-                try:
-                    state.play((x, y, i, j))
-                    self.update_button_text(
-                        xyij, X_CHAR if state.next_player==2 else O_CHAR)
-                    self.update_button_color(
-                        xyij, X_COLOR if state.next_player==2 else O_COLOR, None)
-                    self.update_area_color((x, y), None, *BACKGROUND_COLORS)
-                    if state.next_area is not None:
-                        self.update_area_color(state.next_area, None, NEXTG_COLOR, NEXTG_COLOR)
-                    
-                    # TODO
-                    print(state)
-                except (BoardWonException, AreaWonException, AreaWrongException, CellPlayedException) as e:
-                    print(e)  # TODO
+                xyij = tuple(map(int, event[-4:]))
+                self.play(xyij)
 
         self.window.close()
 
