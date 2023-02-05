36fa3111b6400ae0b7d69660f1a058ac18a9659f
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2023-02-05 19:29:43 +0700

    even larger and longer!

--- ultimatettt/OthelloNeuralNet.py
+++ ultimatettt/OthelloNeuralNet.py
@@ -25,20 +25,20 @@ class OthelloNNet(nn.Module):
         self.bn3 = nn.BatchNorm2d(args.num_channels)
         self.bn4 = nn.BatchNorm2d(args.num_channels)
 
-        # self.fc1 = nn.Linear(args.num_channels*(self.board_x-4)*(self.board_y-4), 1024)
-        self.fc1 = nn.Linear(args.num_channels*(self.board_x-4)*(self.board_y-4), 256)
-        # self.fc_bn1 = nn.BatchNorm1d(1024)
-        self.fc_bn1 = nn.BatchNorm1d(256)
+        self.fc1 = nn.Linear(args.num_channels*(self.board_x-4)*(self.board_y-4), 1024)
+        # self.fc1 = nn.Linear(args.num_channels*(self.board_x-4)*(self.board_y-4), 256)
+        self.fc_bn1 = nn.BatchNorm1d(1024)
+        # self.fc_bn1 = nn.BatchNorm1d(256)
 
-        # self.fc2 = nn.Linear(1024, 512)
-        self.fc2 = nn.Linear(256, 128)
-        # self.fc_bn2 = nn.BatchNorm1d(512)
-        self.fc_bn2 = nn.BatchNorm1d(128)
+        self.fc2 = nn.Linear(1024, 512)
+        # self.fc2 = nn.Linear(256, 128)
+        self.fc_bn2 = nn.BatchNorm1d(512)
+        # self.fc_bn2 = nn.BatchNorm1d(128)
 
-        # self.fc3 = nn.Linear(512, self.action_size)
-        self.fc3 = nn.Linear(128, self.action_size)
-        # self.fc4 = nn.Linear(512, 1)
-        self.fc4 = nn.Linear(128, 1)
+        self.fc3 = nn.Linear(512, self.action_size)
+        # self.fc3 = nn.Linear(128, self.action_size)
+        self.fc4 = nn.Linear(512, 1)
+        # self.fc4 = nn.Linear(128, 1)
 
 
 
--- ultimatettt/main.py
+++ ultimatettt/main.py
@@ -14,17 +14,17 @@ coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
 
 args = dotdict({
     # 'numIters': 1000,
-    'numIters': 10,
+    'numIters': 100,
     # 'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
-    'numEps': 12,
+    'numEps': 20,
     'tempThreshold': 15,        #
     'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     # 'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
-    'maxlenOfQueue': 1000, 
+    'maxlenOfQueue': 50000, 
     # 'numMCTSSims': 25,          # Number of games moves for MCTS to simulate.
-    'numMCTSSims': 15,   
+    'numMCTSSims': 20,   
     # 'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
-    'arenaCompare': 20,
+    'arenaCompare': 30,
     'cpuct': 1,
 
     'checkpoint': './temp/',
