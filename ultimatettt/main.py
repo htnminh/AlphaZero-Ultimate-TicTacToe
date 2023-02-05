e893576c4c1a5e0371bf98a5dbfa519dc645c153
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2023-02-05 21:15:46 +0700

    reduce number of iters to test

--- ultimatettt/main.py
+++ ultimatettt/main.py
@@ -14,7 +14,7 @@ coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
 
 args = dotdict({
     # 'numIters': 1000,
-    'numIters': 100,
+    'numIters': 10,
     # 'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
     'numEps': 20,
     'tempThreshold': 15,        #
