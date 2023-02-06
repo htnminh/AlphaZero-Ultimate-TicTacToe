91ac03f560ea69a84f6f69721d99289aa766ce84
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2023-02-06 15:19:44 +0700

    adjust some params, adjust layers

--- ultimatettt/implemented_NeuralNet.py
+++ ultimatettt/implemented_NeuralNet.py
@@ -14,12 +14,10 @@ from OthelloNeuralNet import OthelloNNet as onnet
 
 args = dotdict({
     'lr': 0.001,
-    # 'dropout': 0.3,
-    'dropout': 0.2,
+    'dropout': 0.3,
     # 'epochs': 10,
-    'epochs': 20,
-    # 'batch_size': 64,
-    'batch_size': 128,
+    'epochs': 15,
+    'batch_size': 64,
     'cuda': torch.cuda.is_available(),
     'num_channels': 512,
     })
--- ultimatettt/main.py
+++ ultimatettt/main.py
@@ -14,17 +14,19 @@ coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
 
 args = dotdict({
     # 'numIters': 1000,
-    'numIters': 5,
+    'numIters': 20,
     # 'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
-    'numEps': 25,
-    'tempThreshold': 15,        #
-    'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
+    'numEps': 20,
+    # 'tempThreshold': 15,        #
+    'tempThreshold': 5,
+    # 'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
+    'updateThreshold': 0.55,
     # 'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
     'maxlenOfQueue': 50000, 
     # 'numMCTSSims': 25,          # Number of games moves for MCTS to simulate.
     'numMCTSSims': 20,   
     # 'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
-    'arenaCompare': 30,
+    'arenaCompare': 24,
     'cpuct': 1,
 
     'checkpoint': './temp/',
