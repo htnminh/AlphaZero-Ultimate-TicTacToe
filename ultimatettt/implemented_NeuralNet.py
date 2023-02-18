58aad36c02c99bb112c5fb3e153c90579d6ce699
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2023-02-18 19:51:02 +0700

    finish adding a masking channel

--- ultimatettt/implemented_NeuralNet.py
+++ ultimatettt/implemented_NeuralNet.py
@@ -16,7 +16,8 @@ args = dotdict({
     'lr': 0.001,
     'dropout': 0.3,
     # 'epochs': 10,
-    'epochs': 15,
+    # 'epochs': 15,
+    'epochs': 2,  # TEST
     'batch_size': 64,
     'cuda': torch.cuda.is_available(),
     # 'num_channels': 512,
@@ -34,7 +35,7 @@ class NNetWrapper(NeuralNet):
 
     def train(self, examples):
         """
-        examples: list of examples, each example is of form (board, pi, v) (+ curr_area)
+        examples: list of examples, each example is of form (board, pi, v) (+ curr_area) (+ mask_2d)
         """
         optimizer = optim.Adam(self.nnet.parameters())
 
@@ -52,21 +53,23 @@ class NNetWrapper(NeuralNet):
             print(f'batch_no: batch_count={batch_count}')
             for batch_no in range(batch_count):
                 sample_ids = np.random.randint(len(examples), size=args.batch_size)
-                boards, pis, vs, curr_areas = list(zip(*[examples[i] for i in sample_ids]))
+                boards, pis, vs, curr_areas, mask_2ds = list(zip(*[examples[i] for i in sample_ids]))
                 boards = torch.FloatTensor(np.array(boards).astype(np.float64))
                 target_pis = torch.FloatTensor(np.array(pis))
                 target_vs = torch.FloatTensor(np.array(vs).astype(np.float64))
+                mask_2ds = torch.FloatTensor(np.array(mask_2ds).astype(np.float64))
                 # target_curr_areas = torch.FloatTensor(np.array(curr_areas).astype(np.float64))
 
                 # predict
                 if args.cuda:
                     # boards, target_pis, target_vs, curr_areas = \
-                    boards, target_pis, target_vs = \
-                        boards.contiguous().cuda(), target_pis.contiguous().cuda(), target_vs.contiguous().cuda()#, target_curr_areas.contiguous().cuda()
+                    boards, target_pis, target_vs, mask_2ds = \
+                        boards.contiguous().cuda(), target_pis.contiguous().cuda(), \
+                        target_vs.contiguous().cuda(), mask_2ds.contiguous().cuda()
 
 
                 # compute output
-                out_pi, out_v = self.nnet(boards)
+                out_pi, out_v = self.nnet(torch.cat((boards, mask_2ds), 1))
                 l_pi = self.loss_pi(target_pis, out_pi)
                 l_v = self.loss_v(target_vs, out_v)
                 total_loss = l_pi + l_v
--- ultimatettt/main.py
+++ ultimatettt/main.py
@@ -15,11 +15,11 @@ log = logging.getLogger(__name__)
 coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
 
 args = dotdict({
-    # 'numIters': 1000,
-    'numIters': 12,
+    # 'numIters': 12,
+    'numIters': 1,  # TEST
     # 'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
     # 'numEps': 20,
-    'numEps': 2,  # TEST
+    'numEps': 1,  # TEST
     # 'tempThreshold': 15,        #
     'tempThreshold': 5,
     # 'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
@@ -27,9 +27,11 @@ args = dotdict({
     # 'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
     'maxlenOfQueue': 50000, 
     # 'numMCTSSims': 25,          # Number of games moves for MCTS to simulate.
-    'numMCTSSims': 20,   
+    # 'numMCTSSims': 20,   
+    'numMCTSSims': 5,  # TEST 
     # 'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
-    'arenaCompare': 24,
+    # 'arenaCompare': 24,  
+    'arenaCompare': 2,  # TEST
     'cpuct': 1,
 
     'checkpoint': './temp/',
